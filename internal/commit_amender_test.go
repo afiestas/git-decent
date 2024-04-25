e999f735dee88ee5b63cfb49c68858888cae3d8f
Àlex Fiestas <afiestas@kde.org>
2024-04-25 19:28:07 +0200

    Massively simplify commit_maneded_test
    We've turned the test into a table-driven test which perfectly fits
    our use case.
    
    Now the test has clear in and expected outs which will be helptul to
    add more tests while we rewrite the implementation.

--- internal/commit_amender_test.go
+++ internal/commit_amender_test.go
@@ -1,7 +1,6 @@
 package internal
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
@@ -10,213 +9,119 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func parseDate(dateStr string, t *testing.T) time.Time {
-	layout := "2006-01-02 15:04:05"
-	pTime, err := time.Parse(layout, dateStr)
+func sut(t *testing.T, fixtures amendFixture, decentFrames map[time.Weekday]string) {
+	repo := NewRepositoryBuilder(t).As(Working).WithCommitsWithDates(fixtures.initialDates).MustBuild()
+	log, err := repo.Log()
 	assert.NoError(t, err)
 
-	return pTime
-}
-func TestAmendSingleUndecentCommit(t *testing.T) {
-	historyDates := []time.Time{
-		parseDate("2024-01-28 18:30:00", t),
-	}
-
-	repo := NewRepositoryBuilder(t).As(Working).WithCommitsWithDates(historyDates).MustBuild()
-
-	log, err := repo.Log()
+	schedule, err := config.NewScheduleFromRaw(&config.RawScheduleConfig{Days: decentFrames})
 	require.NoError(t, err)
 
-	commit := log[0]
+	for k, commit := range log {
+		if commit.Prev != nil {
+			commit.Prev = log[k-1]
+		}
+		amended := Amend(*commit, log, schedule)
+		log[k] = &amended
+	}
 
-	raw := config.RawScheduleConfig{
-		Days: map[time.Weekday]string{
+	assertAmendedLog(t, fixtures.amendedDates, log)
+}
+
+func assertAmendedLog(t *testing.T, amendedDates []time.Time, log GitLog) {
+	assert.NotEmpty(t, log)
+
+	for k, commit := range log {
+		assert.Equalf(t,
+			amendedDates[k],
+			commit.Date,
+			"Amdeded commit (%d) is not like expected",
+			k,
+		)
+	}
+}
+
+type amendFixture struct {
+	initialDates []time.Time
+	amendedDates []time.Time
+}
+
+func makeFixtures(t *testing.T, initial []string, amended []string) amendFixture {
+	if len(initial) != len(amended) {
+		t.Errorf("Initial(%d) dates array must be the same sas as amended(%d)", len(initial), len(amended))
+	}
+
+	parseDate := func(dateStr string, t *testing.T) time.Time {
+		layout := "2006-01-02 15:04:05"
+		pTime, err := time.Parse(layout, dateStr)
+		assert.NoError(t, err)
+
+		return pTime
+	}
+
+	fixtures := amendFixture{}
+	for k, date := range initial {
+		fixtures.initialDates = append(fixtures.initialDates, parseDate(date, t))
+		fixtures.amendedDates = append(fixtures.amendedDates, parseDate(amended[k], t))
+	}
+
+	return fixtures
+}
+
+var tests = []struct {
+	name        string                  // name of the test case
+	initial     []string                // initial commit times as strings
+	amended     []string                // expected amended commit times as strings
+	decentSlots map[time.Weekday]string // mapping of weekdays to time slots
+}{
+	{
+		name:    "Amend Single Undecent Commit",
+		initial: []string{"2024-01-28 18:30:00"},
+		amended: []string{"2024-01-29 09:00:00"},
+		decentSlots: map[time.Weekday]string{
 			time.Monday: "09:00/17:00",
 		},
-	}
-	schedule, err := config.NewScheduleFromRaw(&raw)
-	require.NoError(t, err)
-
-	amended := Amend(*commit, log, schedule)
-	assert.NotEqual(t, amended.Date, commit.Date)
-
-	assert.Equal(t, time.Monday, amended.Date.Weekday())
-	assert.Equal(t, 29, amended.Date.Day())
-	assert.Equal(t, 9, amended.Date.Hour())
-	assert.Equal(t, 0, amended.Date.Minute())
-}
-
-func TestAmendTwoCloseUndecentCommits(t *testing.T) {
-	historyDates := []time.Time{
-		parseDate("2024-01-28 18:30:00", t),
-		parseDate("2024-01-28 18:35:00", t),
-	}
-
-	repo := NewRepositoryBuilder(t).As(Working).WithCommitsWithDates(historyDates).MustBuild()
-
-	log, err := repo.Log()
-	require.NoError(t, err)
-
-	fmt.Println(log)
-	commit := log[0]
-
-	raw := config.RawScheduleConfig{
-		Days: map[time.Weekday]string{
+	},
+	{
+		name:    "Amend Two Close Undecent Commits",
+		initial: []string{"2024-01-28 18:30:00", "2024-01-28 18:35:00"},
+		amended: []string{"2024-01-29 09:00:00", "2024-01-29 09:05:00"},
+		decentSlots: map[time.Weekday]string{
 			time.Monday: "09:00/17:00",
 		},
-	}
-	schedule, err := config.NewScheduleFromRaw(&raw)
-	require.NoError(t, err)
-
-	amended := Amend(*log[0], log, schedule)
-	log[0] = &amended
-	log[1].Prev = log[0]
-	amended2 := Amend(*log[1], log, schedule)
-
-	assert.NotEqual(t, amended.Date, commit.Date)
-	assert.Equal(t, time.Monday, amended.Date.Weekday())
-	assert.Equal(t, 29, amended.Date.Day())
-	assert.Equal(t, 9, amended.Date.Hour())
-	assert.Equal(t, 0, amended.Date.Minute())
-
-	assert.Equal(t, time.Monday, amended2.Date.Weekday())
-	assert.Equal(t, 29, amended2.Date.Day())
-	assert.Equal(t, 9, amended2.Date.Hour())
-	assert.Equal(t, 5, amended2.Date.Minute())
-}
-
-func TestAmendCompressionUndecentCommits(t *testing.T) {
-	historyDates := []time.Time{
-		parseDate("2024-01-28 18:30:00", t),
-		parseDate("2024-01-28 23:59:00", t),
-	}
-
-	repo := NewRepositoryBuilder(t).As(Working).WithCommitsWithDates(historyDates).MustBuild()
-
-	log, err := repo.Log()
-	require.NoError(t, err)
-
-	fmt.Println(log)
-	commit := log[0]
-
-	raw := config.RawScheduleConfig{
-		Days: map[time.Weekday]string{
+	},
+	{
+		name:    "Amend Compression Undecent Commits",
+		initial: []string{"2024-01-28 18:30:00", "2024-01-28 23:59:00"},
+		amended: []string{"2024-01-29 09:00:00", "2024-01-29 09:09:00"},
+		decentSlots: map[time.Weekday]string{
 			time.Monday: "09:00/17:00",
 		},
-	}
-	schedule, err := config.NewScheduleFromRaw(&raw)
-	require.NoError(t, err)
-
-	amended := Amend(*log[0], log, schedule)
-	log[0] = &amended
-	log[1].Prev = log[0]
-	amended2 := Amend(*log[1], log, schedule)
-
-	assert.NotEqual(t, amended.Date, commit.Date)
-	assert.Equal(t, time.Monday, amended.Date.Weekday())
-	assert.Equal(t, 29, amended.Date.Day())
-	assert.Equal(t, 9, amended.Date.Hour())
-	assert.Equal(t, 0, amended.Date.Minute())
-
-	assert.Equal(t, time.Monday, amended2.Date.Weekday())
-	assert.Equal(t, 29, amended2.Date.Day())
-	assert.Equal(t, 9, amended2.Date.Hour())
-	assert.Equal(t, 9, amended2.Date.Minute())
-}
-
-func TestAmendCommitInAmendedRange(t *testing.T) {
-	historyDates := []time.Time{
-		parseDate("2024-01-28 18:30:00", t),
-		parseDate("2024-01-28 23:59:00", t),
-		parseDate("2024-01-29 09:00:00", t),
-	}
-
-	repo := NewRepositoryBuilder(t).As(Working).WithCommitsWithDates(historyDates).MustBuild()
-
-	log, err := repo.Log()
-	require.NoError(t, err)
-
-	fmt.Println(log)
-	commit := log[0]
-
-	raw := config.RawScheduleConfig{
-		Days: map[time.Weekday]string{
+	},
+	{
+		name:    "Amend Commit In Amended Range",
+		initial: []string{"2024-01-28 18:30:00", "2024-01-28 23:59:00", "2024-01-29 09:00:00"},
+		amended: []string{"2024-01-29 09:00:00", "2024-01-29 09:09:00", "2024-01-29 09:10:00"},
+		decentSlots: map[time.Weekday]string{
 			time.Monday: "09:00/17:00",
 		},
-	}
-	schedule, err := config.NewScheduleFromRaw(&raw)
-	require.NoError(t, err)
-
-	amended := Amend(*log[0], log, schedule)
-	log[0] = &amended
-	log[1].Prev = log[0]
-	amended2 := Amend(*log[1], log, schedule)
-	log[1] = &amended2
-	log[2].Prev = log[1]
-
-	amended3 := Amend(*log[2], log, schedule)
-
-	assert.NotEqual(t, amended.Date, commit.Date)
-	assert.Equal(t, time.Monday, amended.Date.Weekday())
-	assert.Equal(t, 29, amended.Date.Day())
-	assert.Equal(t, 9, amended.Date.Hour())
-	assert.Equal(t, 0, amended.Date.Minute())
-
-	assert.Equal(t, time.Monday, amended2.Date.Weekday())
-	assert.Equal(t, 29, amended2.Date.Day())
-	assert.Equal(t, 9, amended2.Date.Hour())
-	assert.Equal(t, 9, amended2.Date.Minute())
-
-	assert.Equal(t, time.Monday, amended3.Date.Weekday())
-	assert.Equal(t, 29, amended3.Date.Day())
-	assert.Equal(t, 9, amended3.Date.Hour())
-	assert.Equal(t, 10, amended3.Date.Minute())
-}
-
-func TestAmendOverflow(t *testing.T) {
-	historyDates := []time.Time{
-		parseDate("2024-01-29 17:00:00", t),
-		parseDate("2024-01-29 16:55:00", t),
-		parseDate("2024-01-29 17:50:00", t),
-		parseDate("2024-01-29 20:51:00", t),
-	}
-
-	repo := NewRepositoryBuilder(t).As(Working).WithCommitsWithDates(historyDates).MustBuild()
-
-	log, err := repo.Log()
-	require.NoError(t, err)
-
-	fmt.Println(log)
-	commit := log[0]
-
-	raw := config.RawScheduleConfig{
-		Days: map[time.Weekday]string{
+	},
+	{
+		name:    "Amend Overflow",
+		initial: []string{"2024-01-29 17:00:00", "2024-01-29 16:55:00", "2024-01-29 17:50:00", "2024-01-29 20:51:00"},
+		amended: []string{"2024-01-29 17:00:00", "2024-01-29 17:05:00", "2024-01-29 18:00:00", "2024-01-30 09:00:00"},
+		decentSlots: map[time.Weekday]string{
 			time.Monday:  "09:00/17:00, 18:00/19:00",
 			time.Tuesday: "09:00/17:00",
 		},
+	},
+}
+
+func TestAmendCommits(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			fixtures := makeFixtures(t, tc.initial, tc.amended)
+			sut(t, fixtures, tc.decentSlots)
+		})
 	}
-	schedule, err := config.NewScheduleFromRaw(&raw)
-	require.NoError(t, err)
-
-	amended := Amend(*log[0], log, schedule)
-	amended2 := Amend(*log[1], log, schedule)
-	log[1] = &amended2
-	log[2].Prev = log[1]
-	amended3 := Amend(*log[2], log, schedule)
-
-	assert.Equal(t, amended.Date, commit.Date)
-	assert.Equal(t, time.Monday, amended.Date.Weekday())
-	assert.Equal(t, 29, amended.Date.Day())
-	assert.Equal(t, 17, amended.Date.Hour())
-	assert.Equal(t, 0, amended.Date.Minute())
-
-	assert.Equal(t, time.Monday, amended2.Date.Weekday())
-	assert.Equal(t, 29, amended2.Date.Day())
-	assert.Equal(t, 17, amended2.Date.Hour())
-	assert.Equal(t, 5, amended2.Date.Minute())
-
-	assert.Equal(t, time.Monday, amended3.Date.Weekday())
-	assert.Equal(t, 29, amended3.Date.Day())
-	assert.Equal(t, 18, amended3.Date.Hour())
-	assert.Equal(t, 0, amended3.Date.Minute())
 }
